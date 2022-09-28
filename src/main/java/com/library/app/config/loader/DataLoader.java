493ac69a5df3b8f444afce4eb555ab80e6a61cb2
F1iper <ftimofiejew@gmail.com>
2022-09-28 21:49:00 +0200

    Encryption of hardcoded accounts passwords

--- src/main/java/com/library/app/config/loader/DataLoader.java
+++ src/main/java/com/library/app/config/loader/DataLoader.java
@@ -8,6 +8,7 @@ import com.library.app.employee.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,12 +17,13 @@ class DataLoader implements ApplicationRunner {
     
     private final AccountRepository accountRepository;
     private final EmployeeRepository employeeRepository;
+    private final PasswordEncoder passwordEncoder;
     
     public void run(ApplicationArguments args) {
-        accountRepository.save(new Account(1L, "John", "Travolta", "test@test.com", "password", Role.ADMIN, false));
-        accountRepository.save(new Account(2L, "Jacob", "theuberuser", "test@test.eu", "password123", Role.EMPLOYEE, false));
-        accountRepository.save(new Account(3L, "Steven", "brown", "monkey@monkey.pl", "morerandom", Role.EMPLOYEE, false));
-        accountRepository.save(new Account(4L, "Mary", "McKinley", "sheep@sheep.eu", "nullshiet", Role.MEMBER, false));
+        accountRepository.save(new Account(1L, "John", "Travolta", "test@test.com", passwordEncoder.encode("password"), Role.ADMIN, false));
+        accountRepository.save(new Account(2L, "Jacob", "theuberuser", "test@test.eu", passwordEncoder.encode("password123"), Role.EMPLOYEE, false));
+        accountRepository.save(new Account(3L, "Steven", "brown", "monkey@monkey.pl", passwordEncoder.encode("morerandom"), Role.EMPLOYEE, false));
+        accountRepository.save(new Account(4L, "Mary", "McKinley", "sheep@sheep.eu", passwordEncoder.encode("nullshiet"), Role.MEMBER, false));
         
         employeeRepository.save(new Employee(1L, "Jacek", "Mrzonka", 1024L));
         employeeRepository.save(new Employee(2L, "Korwin", "Parufka", 3L));
